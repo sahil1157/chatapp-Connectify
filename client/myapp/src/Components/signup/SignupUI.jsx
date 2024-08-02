8b2b4f6d0b3d5c4c3d3331a036c14106d5a73620
Sahil1157 <143487205+sahil1157@users.noreply.github.com>
2024-08-02 16:24:23 +0545

    new commit

--- client/myapp/src/Components/signup/SignupUI.jsx
+++ client/myapp/src/Components/signup/SignupUI.jsx
@@ -182,89 +182,80 @@ const SignupUI = () => {
     }
 
     return (
-        <div className='w-full sm:px-0 px-[4%] mx-auto mt-5 items-center flex flex-col gap-4'>
-            <div>
-                <p className='sm:text-[38px] text-[28px] font-[700] font-sans text-[#1877F2]'>Connectify</p>
-            </div>
-            <div className='w-full p-3 sm:p-8 max-w-[490px] h-fit flex flex-col gap-4 bg-white shadow-2xl rounded-xl text-start border-[1px]'>
-                <div className='w-full flex flex-row items-center justify-between gap-5'>
-                    <p className='text-lg flex items-center'>Create a new account</p>
-                    <div className='flex items-center lg:cursor-pointer flex-col gap-2'>
-                        <input
-                            type='file'
-                            id='fileInput'
-                            accept='image/jpeg, image/jpg, image/png'
-                            className='hidden'
-                            onChange={handleSubmitImage}
-                        />
-                        <label
-                            htmlFor='fileInput'
-                            className='min-w-[68px] border-[1px] min-h-[68px] max-w-[68px] max-h-[68px] rounded-full overflow-hidden cursor-pointer'
-                        >
-                            {showToUser && showToUser ? (
-                                <img
-                                    src={showToUser}
-                                    alt='Profile'
-                                    className='w-full h-full object-cover rounded-full'
-                                />
-                            ) : <img
-                                src={img}
-                                alt='Profile'
-                                className='w-full h-full object-cover rounded-full'
+        <div className='flex justify-center items-center w-full min-h-screen h-full border-2 place-items-center'>
+            <div className='w-full h-full sm:px-0 px-[4%] mx-auto mt-5 items-center flex flex-col gap-4'>
+                <div className='w-full p-3 sm:p-8 max-w-[500px] h-fit flex flex-col gap-4 bg-white shadow-2xl rounded-xl text-start border-[1px]'>
+                    <div className='w-full flex flex-row items-center justify-between gap-5'>
+                        <div className='flex flex-col'>
+                            <p className='sm:text-[38px] text-[28px] font-[700] font-sans text-[#1877F2]'>Connectify</p>
+                            <p className='text-md text-slate-700 flex items-center'>Create a new account</p>
+                        </div>
+                        <div className='flex items-center lg:cursor-pointer flex-col gap-2'>
+                            <input
+                                type='file'
+                                id='fileInput'
+                                accept='image/jpeg, image/jpg, image/png'
+                                className='hidden'
+                                onChange={handleSubmitImage}
                             />
-                            }
-                        </label>
-                    </div>
+                            <label htmlFor='fileInput' className='flex md:cursor-pointer w-full h-fit justify-center items-center'>
+                                <div className='flex justify-center h-[54px] w-[54px]'>
+                                    <img src={showToUser || img} className='rounded-full w-full h-full object-cover' alt='' />
+                                </div>
+                            </label>
+                            <p className='text-gray-500 font-mono'>update profile</p>
+                        </div>
 
-                </div>
-                <form onSubmit={handleSubmit} className='mt-2 flex gap-3 flex-col'>
-                    <div className="flex gap-3">
-                        <div className="flex flex-col w-full">
-                            <input required type="text" placeholder='First name' className='placeholder:text-neutral-400 p-3 text-black outline-none w-full rounded-lg h-12 border-[1px] border-gray-300' name="firstname" value={inpVal.firstname} onChange={handleChange} onBlur={handleBlur} />
-                            {errors.firstname && <p className="text-sm font-[500] font-sans text-red-500 mt-1">{errors.firstname}</p>}
+                    </div>
+                    <form onSubmit={handleSubmit} className='mt-2 flex gap-3 flex-col'>
+                        <div className="flex gap-3">
+                            <div className="flex flex-col w-full">
+                                <input required type="text" placeholder='First name' className='placeholder:text-neutral-400 p-3 text-black outline-none w-full rounded-lg h-12 border-[1px] border-gray-300' name="firstname" value={inpVal.firstname} onChange={handleChange} onBlur={handleBlur} />
+                                {errors.firstname && <p className="text-xs font-[500] font-sans text-red-500 mt-1">{errors.firstname}</p>}
+                            </div>
+                            <div className="flex flex-col w-full">
+                                <input required type="text" placeholder='Last name' className='placeholder:text-neutral-400 p-3 text-black outline-none w-full rounded-lg h-12 border-[1px] border-gray-300' name="lastname" value={inpVal.lastname} onChange={handleChange} onBlur={handleBlur} />
+                                {errors.lastname && <p className="text-xs font-sans font-[500] text-red-500 mt-1">{errors.lastname}</p>}
+                            </div>
                         </div>
-                        <div className="flex flex-col w-full">
-                            <input required type="text" placeholder='Last name' className='placeholder:text-neutral-400 p-3 text-black outline-none w-full rounded-lg h-12 border-[1px] border-gray-300' name="lastname" value={inpVal.lastname} onChange={handleChange} onBlur={handleBlur} />
-                            {errors.lastname && <p className="text-sm font-sans font-[500] text-red-500 mt-1">{errors.lastname}</p>}
+                        <p className='text-left text-sm font-semibold text-gray-400'>Gender</p>
+                        <div className="sm:flex-row flex flex-col items-center w-full justify-center gap-3 -mt-1">
+                            <button type="button" className='flex justify-between items-center w-full p-3 rounded-lg border-[1px] border-gray-300' onClick={() => setInpVal(prevState => ({ ...prevState, gender: 'male' }))}>
+                                Male
+                                <input required type="radio" name="gender" value="male" checked={inpVal.gender === 'male'} onChange={handleChange} className="form-radio ml-2" />
+                            </button>
+                            <button type="button" className='flex justify-between items-center w-full p-3 rounded-lg border-[1px] border-gray-300' onClick={() => setInpVal(prevState => ({ ...prevState, gender: 'female' }))}>
+                                Female
+                                <input required type="radio" name="gender" value="female" checked={inpVal.gender === 'female'} onChange={handleChange} className="form-radio ml-2" />
+                            </button>
                         </div>
-                    </div>
-                    <p className='text-left text-sm font-semibold text-gray-400'>Gender</p>
-                    <div className="sm:flex-row flex flex-col items-center w-full justify-center gap-3 -mt-1">
-                        <button type="button" className='flex justify-between items-center w-full p-3 rounded-lg border-[1px] border-gray-300' onClick={() => setInpVal(prevState => ({ ...prevState, gender: 'male' }))}>
-                            Male
-                            <input required type="radio" name="gender" value="male" checked={inpVal.gender === 'male'} onChange={handleChange} className="form-radio ml-2" />
+                        <input required type="email" placeholder='Email ' className='placeholder:text-neutral-400 p-3 text-black outline-none w-full rounded-lg h-12 border-[1px] border-gray-300' name="email" value={inpVal.email} onChange={handleChange} onBlur={handleBlur} />
+                        {errors.email && <p className="text-xs font-sans font-[500] text-red-500 mt-1">{errors.email}</p>}
+                        <div className="relative w-full">
+                            <input required type={showPassword ? 'text' : 'password'} placeholder='New password' className='placeholder:text-neutral-400 p-3 text-black outline-none w-full rounded-lg h-12 border-[1px] border-gray-300' name="password" value={inpVal.password} onChange={handleChange} onBlur={handleBlur} />
+                            <span className="absolute right-3 top-3 cursor-pointer" onClick={() => setShowPassword(!showPassword)}>
+                                {!showPassword ? <FaEyeSlash /> : <FaEye />}
+                            </span>
+                        </div>
+                        {errors.file && <p className="text-xs font-sans font-[500] text-red-500 mt-1">{errors.file}</p>}
+                        <button type="submit" className='w-full font-sans h-12 rounded-lg bg-[#1877F2] hover:bg-[#1f6bcd] duration-300 text-white text-xl font-[700] flex justify-center items-center'>
+                            {loading ? (
+                                <div className="w-6 h-6 border-4 border-t-transparent border-white rounded-full animate-spin"></div>
+                            ) : (
+                                'Signin'
+                            )}
                         </button>
-                        <button type="button" className='flex justify-between items-center w-full p-3 rounded-lg border-[1px] border-gray-300' onClick={() => setInpVal(prevState => ({ ...prevState, gender: 'female' }))}>
-                            Female
-                            <input required type="radio" name="gender" value="female" checked={inpVal.gender === 'female'} onChange={handleChange} className="form-radio ml-2" />
-                        </button>
+                    </form>
+                    <p className='text-md md:cursor-pointer text-[#1f63bb]'>Forgot account?</p>
+                    <div className="flex flex-row w-full items-center justify-center">
+                        <hr className="flex-grow border-t-1 items-center flex border-gray-300 mx-2" />
+                        <p>or</p>
+                        <hr className="flex-grow border-t-1 items-center flex border-gray-300 mx-2" />
                     </div>
-                    <input required type="email" placeholder='Email ' className='placeholder:text-neutral-400 p-3 text-black outline-none w-full rounded-lg h-12 border-[1px] border-gray-300' name="email" value={inpVal.email} onChange={handleChange} onBlur={handleBlur} />
-                    {errors.email && <p className="text-sm font-sans font-[500] text-red-500 mt-1">{errors.email}</p>}
-                    <div className="relative w-full">
-                        <input required type={showPassword ? 'text' : 'password'} placeholder='New password' className='placeholder:text-neutral-400 p-3 text-black outline-none w-full rounded-lg h-12 border-[1px] border-gray-300' name="password" value={inpVal.password} onChange={handleChange} onBlur={handleBlur} />
-                        <span className="absolute right-3 top-3 cursor-pointer" onClick={() => setShowPassword(!showPassword)}>
-                            {!showPassword ? <FaEyeSlash /> : <FaEye />}
-                        </span>
-                    </div>
-                    {errors.file && <p className="text-sm font-sans font-[500] text-red-500 mt-1">{errors.file}</p>}
-                    <button type="submit" className='w-full font-sans h-12 rounded-lg bg-[#1877F2] hover:bg-[#1f6bcd] duration-300 text-white text-xl font-[700] flex justify-center items-center'>
-                        {loading ? (
-                            <div className="w-6 h-6 border-4 border-t-transparent border-white rounded-full animate-spin"></div>
-                        ) : (
-                            'Signin'
-                        )}
-                    </button>
-                </form>
-                <p className='text-md md:cursor-pointer text-[#1f63bb]'>Forgot account?</p>
-                <div className="flex flex-row w-full items-center justify-center">
-                    <hr className="flex-grow border-t-1 items-center flex border-gray-300 mx-2" />
-                    <p>or</p>
-                    <hr className="flex-grow border-t-1 items-center flex border-gray-300 mx-2" />
+                    <p onClick={() => navigate('/login')} className='text-md md:cursor-pointer text-[#1f63bb]'>Already have an account?</p>
                 </div>
-                <p onClick={() => navigate('/login')} className='text-md md:cursor-pointer text-[#1f63bb]'>Already have an account?</p>
-            </div>
-        </div >
+            </div >
+        </div>
     )
 }
 
